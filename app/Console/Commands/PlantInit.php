7eef5a9b576c5071f3b000ae3899691237ce74c6
CoooooLer <1393970294@qq.com>
2018-04-19 09:59:30 +0800

    更改command类名

--- app/Console/Commands/PlantInit.php
+++ app/Console/Commands/PlantInit.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
 
-class initPlant extends Command
+class plantInit extends Command
 {
     /**
      * The name and signature of the console command.
